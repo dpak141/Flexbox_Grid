f1a6f5c3c6844b270993c8eaf04c76b31b722884
dpak141 <dpak141@gmail.com>
2023-08-10 16:42:44 +0545

    Await

--- Fetch/Async and Await/index.js
+++ Fetch/Async and Await/index.js
@@ -7,54 +7,27 @@ let stocks = {
 
 let is_shop_open = true;
 
-let order = (time, work) => {
+let toppings_choice = () => {
   return new Promise((resolve, reject) => {
-    if (is_shop_open) {
-      setTimeout(() => {
-        resolve(work());
-      }, time);
-    } else {
-      reject(console.log("Sorry!, Our shop is closed."));
-    }
+    setTimeout(() => {
+      resolve(console.log("Which toppings would you love?"));
+    }, 3000);
   });
 };
 
-order(2000, () => console.log(`${stocks.Fruits[0]} was selected.`))
-  .then(() => {
-    return order(0, () => console.log("Production has started."));
-  })
+async function kitchen() {
+  console.log(" a ");
+  console.log(" b ");
 
-  .then(() => {
-    return order(2000, () => console.log("The fruit was chopped."));
-  })
+  await toppings_choice();
 
-  .then(() => {
-    return order(1000, () =>
-      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added.`)
-    );
-  })
+  console.log(" c ");
+  console.log(" d ");
+  console.log(" e ");
+}
 
-  .then(() => {
-    return order(1000, () => console.log("Start the machine"));
-  })
+kitchen();
 
-  .then(() => {
-    return order(2000, () =>
-      console.log(`icecrem ${stocks.holder[0]} was placed`)
-    );
-  })
-
-  .then(() => {
-    return order(3000, () => console.log(`${stocks.toppings[0]} was spinkled`));
-  })
-
-  .then(() => {
-    return order(1000, () => console.log("Icream was served."));
-  })
-
-  .catch(() => {
-    console.log("Customer Left");
-  })
-  .finally(() => {
-    console.log("Day ended shop is closed");
-  });
+console.log("Doing the dishes");
+console.log("Cleaning the tables");
+console.log("Taking others order");
