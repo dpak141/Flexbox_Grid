2da5a5d3f45c21c0627d2f402bba678fca1120e4
dpak141 <dpak141@gmail.com>
2023-08-10 16:21:54 +0545

    Promises

--- Fetch/Async and Await/index.js
+++ Fetch/Async and Await/index.js
@@ -5,35 +5,56 @@ let stocks = {
   toppings: ["chocolate", "peanuts", "kaju"],
 };
 
-let order = (Fruit_name, call_production) => {
-  setTimeout(() => {
-    console.log(`${stocks.Fruits[Fruit_name]} was selected`);
-    call_production();
-  }, 2000);
-};
+let is_shop_open = true;
 
-let production = () => {
-  setTimeout(() => {
-    console.log("Production has started.");
-    setTimeout(() => {
-      console.log("Food has been chopped.");
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
       setTimeout(() => {
-        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
-        setTimeout(() => {
-          console.log("machine was started");
-          setTimeout(() => {
-            console.log(`icecream was placed on ${stocks.holder[0]}`);
-            setTimeout(() => {
-              console.log(`${stocks.toppings[0]} was added`);
-              setTimeout(() => {
-                console.log("Serve Ice cream");
-              }, 2000);
-            }, 3000);
-          }, 2000);
-        }, 1000);
-      }, 1000);
-    }, 2000);
-  }, 0);
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log("Sorry!, Our shop is closed."));
+    }
+  });
 };
 
-order(1, production);
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected.`))
+  .then(() => {
+    return order(0, () => console.log("Production has started."));
+  })
+
+  .then(() => {
+    return order(2000, () => console.log("The fruit was chopped."));
+  })
+
+  .then(() => {
+    return order(1000, () =>
+      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added.`)
+    );
+  })
+
+  .then(() => {
+    return order(1000, () => console.log("Start the machine"));
+  })
+
+  .then(() => {
+    return order(2000, () =>
+      console.log(`icecrem ${stocks.holder[0]} was placed`)
+    );
+  })
+
+  .then(() => {
+    return order(3000, () => console.log(`${stocks.toppings[0]} was spinkled`));
+  })
+
+  .then(() => {
+    return order(1000, () => console.log("Icream was served."));
+  })
+
+  .catch(() => {
+    console.log("Customer Left");
+  })
+  .finally(() => {
+    console.log("Day ended shop is closed");
+  });
